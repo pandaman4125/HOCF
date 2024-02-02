94309c49836087cd8ebacd0416fdd75a2a83c44f
Chase Groom <groom.chase@gmail.com>
2024-02-01 20:48:32 -0700

    All

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -1,6 +1,6 @@
 /*!
 * Start Bootstrap - Grayscale v7.0.6 (https://startbootstrap.com/theme/grayscale)
-* Copyright 2013-2023 Start Bootstrap
+* Copyright 2013-2024 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-grayscale/blob/master/LICENSE)
 */
 //
