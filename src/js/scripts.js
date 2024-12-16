98891ac0f406c00d743f1af5f83488cc471e96c1
Jeff Groom <jgroom@ciena.com>
2024-12-16 16:09:04 -0700

    add stats

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,51 +2,73 @@
 // Scripts
 // 
 
-window.addEventListener('DOMContentLoaded', event => {
+window.addEventListener("DOMContentLoaded", (event) => {
+  // Navbar shrink function
+  var navbarShrink = function () {
+    const navbarCollapsible = document.body.querySelector("#mainNav");
+    if (!navbarCollapsible) {
+      return;
+    }
+    if (window.scrollY === 0) {
+      navbarCollapsible.classList.remove("navbar-shrink");
+    } else {
+      navbarCollapsible.classList.add("navbar-shrink");
+    }
+  };
 
-    // Navbar shrink function
-    var navbarShrink = function () {
-        const navbarCollapsible = document.body.querySelector('#mainNav');
-        if (!navbarCollapsible) {
-            return;
-        }
-        if (window.scrollY === 0) {
-            navbarCollapsible.classList.remove('navbar-shrink')
-        } else {
-            navbarCollapsible.classList.add('navbar-shrink')
-        }
+  // Shrink the navbar
+  navbarShrink();
 
-    };
+  // Shrink the navbar when page is scrolled
+  document.addEventListener("scroll", navbarShrink);
 
-    // Shrink the navbar 
-    navbarShrink();
-
-    // Shrink the navbar when page is scrolled
-    document.addEventListener('scroll', navbarShrink);
-
-    // Activate Bootstrap scrollspy on the main nav element
-    const mainNav = document.body.querySelector('#mainNav');
-    if (mainNav) {
-        new bootstrap.ScrollSpy(document.body, {
-            target: '#mainNav',
-            rootMargin: '0px 0px -40%',
-        });
-    };
-
-    // Collapse responsive navbar when toggler is visible
-    const navbarToggler = document.body.querySelector('.navbar-toggler');
-    const responsiveNavItems = [].slice.call(
-        document.querySelectorAll('#navbarResponsive .nav-link')
-    );
-
-    responsiveNavItems.map(function (responsiveNavItem) {
-        responsiveNavItem.addEventListener('click', () => {
-            // Check if the clicked item is not the dropdown toggle for "About Us"
-            if (responsiveNavItem.getAttribute('href') !== '#aboutDropdown') {
-                if (window.getComputedStyle(navbarToggler).display !== 'none') {
-                    navbarToggler.click();
-                }
-            }
-        });
+  // Activate Bootstrap scrollspy on the main nav element
+  const mainNav = document.body.querySelector("#mainNav");
+  if (mainNav) {
+    new bootstrap.ScrollSpy(document.body, {
+      target: "#mainNav",
+      rootMargin: "0px 0px -40%",
     });
+  }
+
+  // Collapse responsive navbar when toggler is visible
+  const navbarToggler = document.body.querySelector(".navbar-toggler");
+  const responsiveNavItems = [].slice.call(
+    document.querySelectorAll("#navbarResponsive .nav-link")
+  );
+
+  responsiveNavItems.map(function (responsiveNavItem) {
+    responsiveNavItem.addEventListener("click", () => {
+      // Check if the clicked item is not the dropdown toggle for "About Us"
+      if (responsiveNavItem.getAttribute("href") !== "#aboutDropdown") {
+        if (window.getComputedStyle(navbarToggler).display !== "none") {
+          navbarToggler.click();
+        }
+      }
+    });
+  });
+
+  async function fetchAndDisplayData() {
+    try {
+      const response = await fetch(
+        "https://script.google.com/macros/s/AKfycbyHBtDryEI8y8h2ggHfihs6nBX-KsZUFLgMZDxN2Ld0o1dXpEDMQ6yA3GzAbFjy1kzNGQ/exec"
+      );
+      const values = await response.json();
+      if (values.length) {
+        document.querySelector("#sheetRow1").textContent = `${values[0][0]}`;
+        document.querySelector("#sheetRow1v").textContent = `${values[0][1]}`;
+        document.querySelector("#sheetRow2").textContent = `${values[1][0]}`;
+        document.querySelector("#sheetRow2v").textContent = `${values[1][1]}`;
+        document.querySelector("#sheetRow3").textContent = `${values[2][0]}`;
+        document.querySelector("#sheetRow3v").textContent = `${values[2][1]}`;
+        document.querySelector("#sheetRow4").textContent = `${values[3][0]}`;
+        document.querySelector("#sheetRow4v").textContent = `${values[3][1]}`;
+      } else {
+        console.log("No data found.");
+      }
+    } catch (error) {
+      console.error("Error fetching data from Google Sheets:", error);
+    }
+  }
+  fetchAndDisplayData();
 });
