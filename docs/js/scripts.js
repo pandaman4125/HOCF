4a93cfdfc04fe5fefa840963909ff7d0065091c3
Jeff Groom <jgroom@ciena.com>
2025-02-18 06:45:37 -0600

    release schedule beta

--- docs/js/scripts.js
+++ docs/js/scripts.js
@@ -53,27 +53,4 @@ window.addEventListener("DOMContentLoaded", (event) => {
     });
   });
 
-  async function fetchAndDisplayData() {
-    try {
-      const response = await fetch(
-        "https://script.google.com/macros/s/AKfycbyHBtDryEI8y8h2ggHfihs6nBX-KsZUFLgMZDxN2Ld0o1dXpEDMQ6yA3GzAbFjy1kzNGQ/exec"
-      );
-      const values = await response.json();
-      if (values.length) {
-        document.querySelector("#sheetRow1").textContent = `${values[0][0]}`;
-        document.querySelector("#sheetRow1v").textContent = `${values[0][1]}`;
-        document.querySelector("#sheetRow2").textContent = `${values[1][0]}`;
-        document.querySelector("#sheetRow2v").textContent = `${values[1][1]}`;
-        document.querySelector("#sheetRow3").textContent = `${values[2][0]}`;
-        document.querySelector("#sheetRow3v").textContent = `${values[2][1]}`;
-        document.querySelector("#sheetRow4").textContent = `${values[3][0]}`;
-        document.querySelector("#sheetRow4v").textContent = `${values[3][1]}`;
-      } else {
-        console.log("No data found.");
-      }
-    } catch (error) {
-      console.error("Error fetching data from Google Sheets:", error);
-    }
-  }
-  fetchAndDisplayData();
 });
